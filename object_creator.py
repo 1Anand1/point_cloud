977fad2d77dc376739047152b13bacb4cf4e2b0a
1Anand1 <anand22dandavate@gmail.com>
2025-03-15 17:31:00 +0530

    Created Python files and reorganised the folder struture.

---
+++ object_creator.py
@@ -0,0 +1,32 @@
+# Importing the required libraries
+import numpy as np
+
+# Defining a function to create a 2D or 3D numpy array of X,Y,Z Coordinates
+def func_to_create_object(n:int,low_range:int,high_range:int,z=True)->np.array:
+  """
+  This is the function that creates a numpy array of shape (n,2) or (n,3)
+
+  args :
+    n parameter --> Number of data points
+    low_range parameter --> Tells about the lower range in which the numbers will be distributed
+    high_range parameter --> Tells about the higher range in which the numbers will be distributed
+    z parameter --> If this parameter is True then a 3D array will be created, if False then it will create a 2D array
+
+  returns :
+    It returns a 2D or 3D array 
+  
+  """
+
+  # Making a sorted numpy array of x & y
+  x_range=np.sort(np.random.uniform(low_range,high_range,n))
+  y_range=np.sort(np.random.uniform(low_range,high_range,n))
+
+  # If z is set True then z array will be created
+  if z==True:
+    z_range=np.sort(np.random.uniform(low_range,high_range,n))
+    rqd_array=np.stack((x_range,y_range,z_range),axis=1) # Stacking the x , y & z arrays
+  else:
+    rqd_array=np.stack((x_range,y_range),axis=1) # Stacking the x & y arrays only
+
+  return rqd_array
+func_to_create_object(200,10,200)
