5a1092f3da136254a293c2c01550cb51dcd38060
1Anand1 <anand22dandavate@gmail.com>
2025-03-15 20:02:10 +0530

    Chaning the way in which y & z dimension arrays are created.

--- object_creator.py
+++ object_creator.py
@@ -17,13 +17,13 @@ def func_to_create_object(n:int,low_range:int,high_range:int,z=True)->np.array:
   
   """
 
-  # Making a sorted numpy array of x & y
+  # Making a sorted numpy array of x
   x_range=np.sort(np.random.uniform(low_range,high_range,n))
-  y_range=np.sort(np.random.uniform(low_range,high_range,n))
+  y_range=x_range**2 # Making y a square of x array
 
   # If z is set True then z array will be created
   if z==True:
-    z_range=np.sort(np.random.uniform(low_range,high_range,n))
+    z_range=x_range**3 # Making z a cube of x array
     rqd_array=np.stack((x_range,y_range,z_range),axis=1) # Stacking the x , y & z arrays
   else:
     rqd_array=np.stack((x_range,y_range),axis=1) # Stacking the x & y arrays only
